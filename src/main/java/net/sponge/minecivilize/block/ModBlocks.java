34d0ebe97ef69371b0525ea98e97fb2935a3ed6e
SpongeGamer <228Loser228@gmail.com>
2025-02-27 19:17:18 +0300

    Добавлена новая папка

--- src/main/java/net/sponge/minecivilize/block/ModBlocks.java
+++ src/main/java/net/sponge/minecivilize/block/ModBlocks.java
@@ -1,15 +1,10 @@
 package net.sponge.minecivilize.block;
 
 import java.util.function.Supplier;
-import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.DropExperienceBlock;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -19,22 +14,6 @@ import net.sponge.minecivilize.init.MinecivilizeModItems;
 public class ModBlocks {
     public static final DeferredRegister.Blocks BLOCKS = 
             DeferredRegister.createBlocks(MinecivilizeMod.MODID);
-
-    public static final DeferredBlock<Block> TIN_ORE = registerBlock("tin_ore",
-            () -> new DropExperienceBlock(UniformInt.of(2, 4),
-                    BlockBehaviour.Properties.of()
-                            .mapColor(MapColor.STONE)
-                            .requiresCorrectToolForDrops()
-                            .strength(3f)
-                            .sound(SoundType.STONE)));
-
-    public static final DeferredBlock<Block> DEEPSLATE_TIN_ORE = registerBlock("deepslate_tin_ore",
-            () -> new DropExperienceBlock(UniformInt.of(3, 6),
-                    BlockBehaviour.Properties.of()
-                            .mapColor(MapColor.DEEPSLATE)
-                            .requiresCorrectToolForDrops()
-                            .strength(4.5f)
-                            .sound(SoundType.DEEPSLATE)));
 
     private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
         DeferredBlock<T> toReturn = BLOCKS.register(name, block);
